5a6452e7456a2d7a11a21111d5114720d80e3deb
Котов Иван <kotov038@gmail.com>
2023-09-11 18:32:03 +0300

    CXX17 support

--- model_utils/model.hpp
+++ model_utils/model.hpp
@@ -3,7 +3,9 @@
 #include "pin.hpp"
 
 #include <vsm.hpp>
+#if __cplusplus >= 202002L
 #include <ranges>
+#endif
 
 namespace vsm
 {
@@ -18,7 +20,7 @@ namespace vsm
         VOID actuate(REALTIME time, ACTIVESTATE newstate) override;
         BOOL indicate(REALTIME time, ACTIVEDATA *newstate) override;
         VOID callback(ABSTIME time, EVENTID eventid) override;
-
+#if __cplusplus >= 202002L
         template <std::ranges::output_range<const vsm::pin &> Rng>
         static void init_pins(IINSTANCE *instance, Rng &&rng, std::string prefix, size_t start_value = 0)
         {
@@ -39,7 +41,28 @@ namespace vsm
 
             return res;
         }
+#else
+        template <typename Container>
+        static void init_pins(IINSTANCE *instance, Container &&container, std::string prefix, size_t start_value = 0)
+        {
+            size_t i = 0;
+            for (auto &pin : container)
+                pin.init(instance, prefix + std::to_string((i++) + start_value));
+        }
 
+        template <typename Container>
+        static uint32_t make_number(Container &&container)
+        {
+            uint32_t res = 0;
+            for (auto it = container.rbegin(); it != container.rend(); ++it)
+            {
+                res <<= 1;
+                res += *it;
+            }
+
+            return res;
+        }
+#endif
     public:
         model() = default;
 
